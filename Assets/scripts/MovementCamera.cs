67d7756e6c9ad254b32eb119d4c21d2ffff5e987
Lucas M Lima <lucas_mlima@outlook.com>
2018-04-01 15:32:50 -0300

    Update MovementCamera.cs

--- Assets/scripts/MovementCamera.cs
+++ Assets/scripts/MovementCamera.cs
@@ -5,29 +5,27 @@ using MyllennoPhoneVR;
 
 public class MovementCamera : MonoBehaviour {
 
-    public GameObject cameras;
+	public GameObject cameras;
 	public float SpeedMovement;
 	public float SpeedRotation;
     
 	private CameraVR cameraVR;
     
-    void Start () {
-
-        // inicia a classe de cálculos e os sensores.
-        cameraVR = new CameraVR(true, true, false, SpeedMovement, SpeedRotation, false);
-    }
+    	void Start () {
+        	// inicia a classe de cálculos e os sensores.
+        	cameraVR = new CameraVR(true, true, false, SpeedMovement, SpeedRotation, false);
+    	}
 
 	void Update () {
-
 		// Rotação de câmera.
 		Quaternion quaternion = cameraVR.rotationCamera(cameras.transform.rotation);
-        cameras.transform.rotation = quaternion;
+        	cameras.transform.rotation = quaternion;
 
-        // Movimento de câmera.
-        if (Input.touchCount > 0)
-        {
+        	// Movimento de câmera.
+        	if (Input.touchCount > 0)
+        	{
 			Vector3 movement = cameraVR.movementCamera(cameras.transform.forward);
-            cameras.transform.position += movement;
-        }
-    }
+            		cameras.transform.position += movement;
+        	}
+    	}
 }
